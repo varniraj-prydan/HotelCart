36015ea27823def9c7eb153ddbcf313ce0718b06
sahilsasapara <sahilsasapara.prydan@gmail.com>
2024-12-11 17:39:09 +0530

    tailwind color class added

--- hotelcart/tailwind.config.js
+++ hotelcart/tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Custom colors from your palette
+        'custom-black': '#333333',
+        'custom-white': '#F5F5F5',
+        'light-teal': '#E1F4F3',
+        'grayish-green': '#706C61',
+        'custom-white':'#FAF9F6',
+        'scrollbar-thumb': '#9E090F',
+        'scrollbar-thumb-hover': '#9E090A',
+        'primary-red' : '#9E090F'
+      },
+      boxShadow: {
+        'even': '0 0 5px rgba(0, 0, 0, 0.5)', // Example values
+      },
+    },
   },
   plugins: [],
 }
